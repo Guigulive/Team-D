64a762ed5297117db6b0ce12a93ae8cd3dc9fb8b
Huangbinhqu <Huangbinhqu@gmail.com>
2018-03-17 00:16:05 +0800

    Update 黄斌_35_第二课作业.sol

--- Lesson-2/assignment/黄斌_35_第二课作业.sol
+++ Lesson-2/assignment/黄斌_35_第二课作业.sol
@@ -90,14 +90,14 @@ contract Payroll {
     }
 }
 
-一、第一次消耗gas 24547；
-   第二次消耗gas 25335；
-   第三次消耗gas 26123；
-   第四次消耗gas 26911；
-   第五次消耗gas 27699；
-   第六次消耗gas 28487；
-   第七次消耗gas 29275；
-   第八次消耗gas 30063；
-   第九次消耗gas 30851；
-   第十次消耗gas 31639；每一次gas都不一样，而且gas是递增的。因为function calculateRunway 中for随着新地址的添加，重复运算导致gas不断增加。
+一、一员工消耗gas 24547；
+   二员工消耗gas 25335；
+   三员工消耗gas 26123；
+   四员工消耗gas 26911；
+   五员工消耗gas 27699；
+   六员工消耗gas 28487；
+   七员工消耗gas 29275；
+   八员工消耗gas 30063；
+   九员工消耗gas 30851；
+   十员工消耗gas 31639；每一次gas都不一样，而且gas是递增的。因为function calculateRunway 中for随着新地址的添加，重复运算totalSalary导致gas不断增加。
 二、
