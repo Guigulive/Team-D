fa86376329c7657b636b2c6a2e9c6ebe70ecbe3e
Huangbinhqu <Huangbinhqu@gmail.com>
2018-03-17 00:11:38 +0800

    Update 黄斌_35_第二课作业.sol

--- Lesson-2/assignment/黄斌_35_第二课作业.sol
+++ Lesson-2/assignment/黄斌_35_第二课作业.sol
@@ -1,4 +1,95 @@
 /*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+contract Payroll {
+    struct Employee {
+        address id;
+        uint salary;
+        uint lastPayday;
+    }
+    
+    uint constant payDuration = 10 seconds;
+
+    address owner;
+    Employee[] employees;
+    
+    function Payroll() {
+        owner = msg.sender;
+    }
+    
+    function _partialPaid(Employee employee) private {
+        uint payment = employee.salary * (now - employee.lastPayday) / payDuration;
+        employee.id.transfer(payment);
+    }
+    
+    function _findEmployee(address employeeId) private returns (Employee, uint) {
+        for (uint i = 0; i < employees.length; i++) {
+            if (employees[i].id == employeeId) {
+                return (employees[i], i);
+            }
+        }
+    }
+    
+    function addEmployee(address employeeId, uint salary) {
+        require(msg.sender == owner);
+        
+        var (employee, index) = _findEmployee(employeeId);
+        assert(employee.id == 0x0);
+        
+        employees.push(Employee(employeeId, salary * 1 ether, now));
+    }
+    
+    function removeEmployee(address employeeId) {
+        require(msg.sender == owner);
+        
+        var (employee, index) = _findEmployee(employeeId);
+        assert(employee.id != 0x0);
+        
+        _partialPaid(employee);
+        delete employees[index];
+        employees[index] = employees[employees.length - 1];
+        employees.length -= 1;
+        
+    }
+    
+    function updateEmployee(address employeeId, uint salary) {
+        require(msg.sender == owner);
+        
+        var (employee, index) = _findEmployee(employeeId);
+
+        assert(employee.id != 0x0);
+        _partialPaid(employee);
+        employees[index].salary = salary * 1 ether;
+        employees[index].lastPayday = now;
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    function calculateRunway() returns (uint) {
+        uint totalSalary = 0;
+        for (uint i = 0; i < employees.length; i++) {
+            totalSalary += employees[i].salary;
+        }
+        
+        return this.balance / totalSalary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+    
+    function getPaid() {
+        var (employee, index) = _findEmployee(msg.sender);
+        assert(employee.id != 0x0);
+        
+        uint nextPayday = employee.lastPayday + payDuration;
+        assert(nextPayday < now);
+        employees[index].lastPayday = nextPayday;
+        employees[index].id.transfer(employee.salary);
+    }
+}
+
 一、第一次消耗gas 24547；
    第二次消耗gas 25335；
    第三次消耗gas 26123；
@@ -8,4 +99,5 @@
    第七次消耗gas 29275；
    第八次消耗gas 30063；
    第九次消耗gas 30851；
-   第十次消耗gas 31639；
+   第十次消耗gas 31639；每一次gas都不一样，而且gas是递增的。因为function calculateRunway 中for随着新地址的添加，重复运算导致gas不断增加。
+二、
